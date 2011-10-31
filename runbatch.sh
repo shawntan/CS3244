f19723379f0dd52be5c292b7b85860acf2328588
Shawn Tan <shawn@wtf.sg>
2011-11-01 01:08:56 +0800

    Finally, a better automated training script. Attempting to learn using a better classifier with higher exponents. Some problems due to the quotes I need to use for some

--- runbatch.sh
+++ runbatch.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 J="java -cp /usr/share/java/weka/weka.jar"
 TESTFILE="Formatting/train.arff"
+KERNEL=""
 #$J weka.classifiers.bayes.NaiveBayes	-x 10	-t $TESTFILE -d models/NaiveBayes.default > results/NaiveBayes.default
 tests=(
-	'lazy.IBk'
-	'lazy.IBk -K 5'
-	'lazy.IBk -K 10'
-	'lazy.IBk -K 50'
-	'lazy.IBk -K 100'
-	'functions.SMO'
-	'bayes.NaiveBayes'
-	'bayes.NaiveBayes -K'
+#	'lazy.IBk'
+#	'lazy.IBk -K 5'
+#	'lazy.IBk -K 10'
+#	'lazy.IBk -K 20'
+#	'lazy.IBk -K 30'
+#	'lazy.IBk -K 50'
+#	'lazy.IBk -K 100'
+#	'functions.SMO'
+	'functions.SMO -K "weka.classifiers.functions.supportVector.PolyKernel -E 2.0"'
+#	'bayes.NaiveBayes'
+	'bayes.NaiveBayes -K ' 
 )
 tests_length=${#tests[@]}
 for (( i=0; i<${tests_length}; i++ ));
 do
 	echo ${tests[i]}
-	$J weka.classifiers.${tests[i]} -t $TESTFILE -d models/${tests[i]//[[:space:]]} -x 10 > results/${tests[i]//[[:space:]]} -i 
-	echo "Results for ${tests[i]//[[:space:]]}"
-	tail results/${tests[i]//[[:space:]]}
+	SAVE_FILE=${tests[i]//[[:space:]]}
+	SAVE_FILE=${SAVE_FILE//\"}
+	EXEC_STR="$J weka.classifiers.${tests[i]} -t $TESTFILE -d models/$SAVE_FILE -x 10 > results/$SAVE_FILE -i"
+	echo $EXEC_STR
+	eval $EXEC_STR
+	echo "Results for $SAVE_FILE" 
+	tail -n -20 results/$SAVE_FILE | head -n 10
 done;
 
 #$J weka.classifiers.	-t $TESTFILE -d models/IBk.default > results/IBk.default
