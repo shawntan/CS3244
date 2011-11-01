530cdb078a7e0ef90d8bad457938a3e012272723
Shawn Tan <shawn@wtf.sg>
2011-11-01 09:39:26 +0800

    Results using SVM with -E 1.5 2.0 and 3.0

--- runbatch.sh
+++ runbatch.sh
@@ -6,24 +6,27 @@ KERNEL=""
 tests=(
 #	'lazy.IBk'
 #	'lazy.IBk -K 5'
-#	'lazy.IBk -K 10'
-#	'lazy.IBk -K 20'
+#	'lazy.IBk -K 10 -I'
+#	'lazy.IBk -K 20 -I'
+#	'lazy.IBk -K 10 -F'
+#	'lazy.IBk -K 20 -F'
 #	'lazy.IBk -K 30'
 #	'lazy.IBk -K 50'
 #	'lazy.IBk -K 100'
-#	'functions.SMO'
-	'functions.SMO -K "weka.classifiers.functions.supportVector.PolyKernel -E 2.0"'
+	'functions.SMO'
+#	'functions.SMO -K "weka.classifiers.functions.supportVector.PolyKernel -E 1.5"'
+#	'functions.SMO -K "weka.classifiers.functions.supportVector.PolyKernel -E 2.0"'
+#	'functions.SMO -K "weka.classifiers.functions.supportVector.PolyKernel -E 3.0"'
 #	'bayes.NaiveBayes'
-	'bayes.NaiveBayes -K ' 
+#	'bayes.NaiveBayes -K ' 
 )
 tests_length=${#tests[@]}
 for (( i=0; i<${tests_length}; i++ ));
 do
-	echo ${tests[i]}
+	echo "Doing ${tests[i]}"
 	SAVE_FILE=${tests[i]//[[:space:]]}
 	SAVE_FILE=${SAVE_FILE//\"}
 	EXEC_STR="$J weka.classifiers.${tests[i]} -t $TESTFILE -d models/$SAVE_FILE -x 10 > results/$SAVE_FILE -i"
-	echo $EXEC_STR
 	eval $EXEC_STR
 	echo "Results for $SAVE_FILE" 
 	tail -n -20 results/$SAVE_FILE | head -n 10
